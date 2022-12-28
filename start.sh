ea82056019ed5aa5c709c7ae3b7eea569a4db3dd
colorone123 <97018358+colorone123@users.noreply.github.com>
2022-12-28 06:39:08 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 export UUID=710c24f5-2299-40c6-888b-b27eaf5f8d60
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.lovelytab.com
+export ProxySite=www.bing.com
 
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
